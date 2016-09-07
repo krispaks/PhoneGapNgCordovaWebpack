baee4d96658c671aeef46a581618467c6740c884
Khris Allan Paca <krispaks@yahoo.com>
2016-09-06 20:20:12 -0500

    add a calendar modal initial

---
+++ www/app/components/shared/calendar-modal/calendar-modal.component.js
@@ -0,0 +1,8 @@
+import CalendarModalController from './calendar-modal.controller';
+export default {
+    name: 'calendarModalComponent',
+    config: {
+        templateUrl: './app/components/shared/calendar-modal/calendar-modal.component.html',
+        controller: [CalendarModalController]
+    }
+}
---
+++ www/app/components/shared/calendar-modal/calendar-modal.controller.js
@@ -0,0 +1,6 @@
+class CalendarModalController{
+    constructor(){
+    }    
+}
+
+export default CalendarModalController;
