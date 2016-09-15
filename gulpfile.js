2f05c5c9cd9b9ef20b92b59b587ba32b797326de
Khris <kpaca@Mac-Air-5.local>
2016-09-15 15:02:18 -0500

    minor updates while working with ios
    on this check in i was able to get registration id for push notification

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,22 @@ const webpack = require('webpack');
 const path = require('path');
 const watch = require('gulp-watch');
 
-gulp.task('webpack', ['clean'], (cb) => {
+// Note: Had to revert from arrow functions. old version of node doesnt seem to understand.
+
+gulp.task('webpack', ['clean'], function(cb){
     const config = require('./webpack.dist.config');
-    webpack(config, () => {
+    webpack(config, function() {
         cb();
     });
 });
 
-gulp.task('clean', (cb) => {
+gulp.task('clean', function(cb) {
     del(['./www/dist'])
-        .then(()=>{
+        .then(function(){
             cb();
         });
 });
 
-gulp.task('watch', ['webpack'], () => {
+gulp.task('watch', ['webpack'], function() {
     gulp.watch('./www/app/**/*.js', ['webpack']);
 });
